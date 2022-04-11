3264ec4241d03fe989420fd066429e2ccd45099e
tomluxton <ta.luxton@gmail.com>
2022-04-11 13:36:40 +1000

    Added buy and sell buttons that are not connected yet and static price and player ownership worth

--- src/components/Marketplace.jsx
+++ src/components/Marketplace.jsx
@@ -4,28 +4,28 @@ import { NavLink } from "react-router-dom";
 function Marketplace() {
   return (
     <div className="Marketplace">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
+      <div className="container">
+        <div className="row align-items-center my-5">
+          <div className="col-lg-7">
             <img
-              class="img-fluid rounded mb-4 mb-lg-0"
+              className="img-fluid rounded mb-4 mb-lg-0"
               src="http://placehold.it/900x400"
               alt=""
             />
           </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Marketplace</h1>
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">Marketplace</h1>
             <p>
               This is where you can buy and sell stocks.
             </p>
           </div>
         </div>
-        <div class="row">
-        <div class="col-sm">
+        <div className="row">
+        <div className="col-sm">
           <NavLink className="nav-link" to = "/marketplace/stock1">Stock 1</NavLink>
         </div>
-        <div class="col-sm">Stock 2</div>
-        <div class="col-sm">Stock 3</div>
+        <div className="col-sm">Stock 2</div>
+        <div className="col-sm">Stock 3</div>
         </div>
       </div>
     </div>
